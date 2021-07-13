97530f46e0e9343a59ee02cef09475cd018432e7
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-07-13 08:53:47 -0600

    added comment

--- UIScrollView_horizontal/ViewController.swift
+++ UIScrollView_horizontal/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+// Add 1 more protocol: UIScrollViewDelegate:
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var contentWidth:CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for image in 0...2{
+            
+            let imageToDisplay = UIImage(named: "\(image).png")
+            
+            let imageView = UIImageView(image: imageToDisplay)
+            
+            let xCoordinate = view.frame.midX + view.frame.width * CGFloat(image)
+            
+            contentWidth += view.frame.width
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: xCoordinate - 50, y: (view.frame.height / 2) - 50, width: 100, height: 100)
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
+        
     }
 
-
+//Add  one function scrollViewDidScroll :
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        print(scrollView.contentOffset)
+        //pageControl.currentPage =
+    }
 }
 
